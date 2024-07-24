68c1f28a2ae8c914cfae71ff5ea2efba8f62c504
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-07-25 00:03:19 +0530

    updated login pages

--- src/components/admin-login/AdminLogin.jsx
+++ src/components/admin-login/AdminLogin.jsx
@@ -71,8 +71,8 @@ const AdminLogin = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-blue-50 via-white to-blue-100">
       {!loading ? (
-        <div className="w-full max-w-md bg-white rounded-lg px-8 py-12 shadow-2xl flex flex-col items-center">
-          <h1 className="text-center text-2xl font-medium mb-8">
+        <div className="lg:w-full md:w-full w-3/4 max-w-md bg-white rounded-lg px-8 py-12 shadow-2xl flex flex-col items-center">
+          <h1 className="text-center lg:text-2xl md:text-2xl text-lg font-medium mb-8">
             Welcome Back Admin!
           </h1>
 
@@ -80,14 +80,14 @@ const AdminLogin = () => {
             <div className="mb-6">
               <label
                 htmlFor="email"
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-xs lg:text-sm md:text-sm font-bold mb-2"
               >
                 Email:
               </label>
               <input
                 type="email"
                 id="email"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
+                className="shadow appearance-none border rounded w-full py-1 lg:py-2 md:py-2 px-2 lg:px-3 md:px-3  text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50 text-sm lg:text-base md:text-base"
                 placeholder="youremail@example.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -98,14 +98,14 @@ const AdminLogin = () => {
             <div className="mb-6">
               <label
                 htmlFor="password"
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700  text-xs lg:text-sm md:text-sm font-bold mb-2"
               >
                 Password:
               </label>
               <input
                 type="password"
                 id="password"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
+                className="shadow appearance-none border rounded w-full py-1 lg:py-2 md:py-2 px-2 lg:px-3 md:px-3  text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50 text-sm lg:text-base md:text-base"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -122,17 +122,17 @@ const AdminLogin = () => {
             <div className="flex items-center mb-4">
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
+                className="w-full lg:w-1/4 md:w-1/4 mt-5 lg:mt-0 md:mt-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 lg:py-2 md:py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
               >
                 Login
               </button>
             </div>
-            <div className="text-red-500 text-sm text-center h-5">
+            <div className="text-red-500 lg:text-sm md:text-sm text-xs  text-center h-5">
               {errorMessage}
             </div>
 
-            <div className="text-center text-gray-500 text-sm">
-              Sign in as Employee
+            <div className="text-center text-gray-500 text-xs lg:text-sm md:text-sm ">
+              Sign in as Employee &nbsp;
               <span
                 className="text-blue-500 font-bold hover:cursor-pointer"
                 onClick={handleEmployeeLogin}
--- src/components/employee-login/EmployeeLogin.jsx
+++ src/components/employee-login/EmployeeLogin.jsx
@@ -72,8 +72,8 @@ const EmployeeLogin = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-blue-50 via-white to-blue-100">
       {!loading ? (
-        <div className="w-full max-w-md bg-white rounded-lg px-8 py-12 shadow-2xl flex flex-col items-center">
-          <h1 className="text-center text-2xl font-medium mb-8">
+        <div className="lg:w-full md:w-full w-3/4 max-w-md bg-white rounded-lg px-8 py-12 shadow-2xl flex flex-col items-center">
+          <h1 className="text-center lg:text-2xl md:text-2xl text-lg font-medium mb-8">
             Welcome Back Employee!
           </h1>
 
@@ -81,32 +81,31 @@ const EmployeeLogin = () => {
             <div className="mb-6">
               <label
                 htmlFor="email"
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-xs lg:text-sm md:text-sm font-bold mb-2"
               >
                 Email:
               </label>
               <input
                 type="email"
                 id="email"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
+                className="shadow appearance-none border rounded w-full py-1 lg:py-2 md:py-2 px-2 lg:px-3 md:px-3  text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50 text-sm lg:text-base md:text-base"
                 placeholder="youremail@example.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
-
             <div className="mb-6">
               <label
                 htmlFor="password"
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700  text-xs lg:text-sm md:text-sm font-bold mb-2"
               >
                 Password:
               </label>
               <input
                 type="password"
                 id="password"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
+                className="shadow appearance-none border rounded w-full py-1 lg:py-2 md:py-2 px-2 lg:px-3 md:px-3  text-gray-700 focus:outline-none focus:ring-blue-500 focus:ring-opacity-50 text-sm lg:text-base md:text-base"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -119,21 +118,19 @@ const EmployeeLogin = () => {
                 Forgot Password?
               </span>
             </div>
-
             <div className="flex items-center mb-4">
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
+                className="w-full lg:w-1/4 md:w-1/4 mt-5 lg:mt-0 md:mt-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 lg:py-2 md:py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
               >
                 Login
               </button>
-            </div>
-            <div className="text-red-500 text-sm text-center h-5">
+            </div>{" "}
+            <div className="text-red-500 lg:text-sm md:text-sm text-xs  text-center h-5">
               {errorMessage}
             </div>
-
-            <div className="text-center text-gray-500 text-sm">
-              Sign up as Employee{" "}
+            <div className="text-center text-gray-500 text-xs lg:text-sm md:text-sm ">
+              Sign up as Employee &nbsp;
               <span
                 onClick={handleAdminContact}
                 className="text-blue-500 font-bold hover:cursor-pointer"
