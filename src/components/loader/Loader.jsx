24b12950a588091b387614017db1d66888a2c22a
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-07-27 08:52:01 +0530

    updated new loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,7 +1,8 @@
+import "ldrs/quantum";
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-100 opacity-75 flex justify-center items-center z-50">
-      <div className="w-16 h-16 rounded-full border-4 border-dashed border-blue-500 animate-spin"></div>
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-100 opacity-90 flex justify-center items-center z-80">
+      <l-quantum size="80" speed="1.75" color="blue"></l-quantum>
     </div>
   );
 };
