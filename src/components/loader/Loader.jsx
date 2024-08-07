1066611ec953beda50e23af780310b558920a13e
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-08-08 02:47:40 +0530

    fixed loader error

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,8 +1,22 @@
-import "ldrs/quantum";
+import "./Loader.css";
 const Loader = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-gray-100 opacity-90 flex justify-center items-center z-80">
-      <l-quantum size="80" speed="1.75" color="blue"></l-quantum>
+      <div className="containerload">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+      </div>
     </div>
   );
 };
